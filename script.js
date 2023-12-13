2e2d941904304cac3a29ff1261b3e69f40e888d4
Math0616 <151874665+Math0616@users.noreply.github.com>
2023-12-13 23:42:36 +0100

    update

--- script.js
+++ script.js
@@ -9,12 +9,19 @@ fetch('https://ordinal-maxi-biz-vercel-project.vercel.app/api/ombcollection')
 		console.error('Error fetching data:', error);
 	});
 
-fetch('https://ordinal-maxi-biz-vercel-project.vercel.app/api/ombtoken')
+fetch('https://ordinal-maxi-biz-vercel-project.vercel.app/api/theapegang')
 	.then(response => response.json())
-	.then(data => {
-		document.getElementById('listed').textContent = data.listed;
-		document.getElementById('listedPrice').textContent = data.listedPrice;
+	.then(tokens => {
+		tokens.forEach(token => {
+		console.log(`Token ID: ${token.id}`);
+		if (token.listed) {
+			console.log(`Listed: Yes`);
+			console.log(`Listed Price: ${token.listedPrice}`);
+		} else {
+			console.log(`Listed: No`);
+		}
+		});
 	})
 	.catch(error => {
-		console.error('Error fetching data:', error);
+		console.error('Error fetching token data:', error);
 	});
