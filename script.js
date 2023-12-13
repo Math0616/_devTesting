a91bf3301551c3b46c0305e17c3bc6028cddae46
Math0616 <151874665+Math0616@users.noreply.github.com>
2023-12-13 21:34:13 +0100

    update

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ fetch('https://ordinal-maxi-biz-vercel-project.vercel.app/api/ombcollection')
 	.catch(error => {
 		console.error('Error fetching data:', error);
 	});
+
+fetch('https://ordinal-maxi-biz-vercel-project.vercel.app/api/ombtoken')
+	.then(response => response.json())
+	.then(data => {
+		document.getElementById('listed').textContent = data.listed;
+		document.getElementById('listedPrice').textContent = data.listedPrice;
+	})
+	.catch(error => {
+		console.error('Error fetching data:', error);
+	});
