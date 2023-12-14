a8c296fb2f98ebd1c39e003dbf67e47fced458a8
Math0616 <151874665+Math0616@users.noreply.github.com>
2023-12-14 09:33:33 +0100

    update

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
 document.addEventListener('DOMContentLoaded', function() {
-	fetch('https://ordinalmaxibiz.vercel.app/api/theapegang') // Replace with your actual endpoint
-		.then(response => response.json())
-		.then(data => {
-			const container = document.getElementById('token-container');
-			data.forEach(group => {
-				group.tokens.forEach(token => {
-					const tokenElement = document.createElement('div');
-					tokenElement.classList.add('token');
+    fetch('https://ordinalmaxibiz.vercel.app/api/theapegang') // Replace with your actual endpoint
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById('token-container');
+            data.forEach(group => {
+                group.tokens.forEach(token => {
+                    const tokenElement = document.createElement('div');
+                    tokenElement.classList.add('token');
 
-					// Add token details
-					tokenElement.innerHTML = `
-						<h3>Token ID: ${token.id}</h3>
-						<p>Listed: ${token.listed ? 'Yes' : 'No'}</p>
-						${token.listed ? `<p>Listed Price: ${token.listedPrice}</p>` : ''}
-					`;
+                    let priceInfo = '';
+                    if (token.listed) {
+                        const formattedPrice = `₿${(token.listedPrice / 100000000).toFixed(2)}`;
+                        priceInfo = `<p>Listed Price: ${formattedPrice}</p>`;
+                    }
 
-					container.appendChild(tokenElement);
-				});
-			});
-		})
-		.catch(error => {
-			console.error('Error fetching token data:', error);
-		});
+                    // Add token details
+                    tokenElement.innerHTML = `
+                        <h3>Token ID: ${token.id}</h3>
+                        <p>Listed: ${token.listed ? 'Yes' : 'No'}</p>
+                        ${priceInfo}
+                    `;
 
-	fetch('https://ordinalmaxibiz.vercel.app/api/ombcollection')
-		.then(response => response.json())
-		.then(data => {
-			document.getElementById('floorPrice').textContent = data.floorPrice;
-			document.getElementById('owners').textContent = data.owners;
-			document.getElementById('totalListed').textContent = data.totalListed;
-		})
-		.catch(error => {
-			console.error('Error fetching data:', error);
-		});	
-});	
-	
+                    container.appendChild(tokenElement);
+                });
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching token data:', error);
+        });
+});
+
+fetch('https://ordinalmaxibiz.vercel.app/api/ombcollection')
+	.then(response => {
+		console.log('Response received:', response);
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+		return response.json();
+	})
+	.then(data => {
+		console.log('Data received:', data);
+		document.getElementById('floorPrice').textContent = data.floorPrice;
+		document.getElementById('owners').textContent = data.owners;
+		document.getElementById('totalListed').textContent = data.totalListed;
+	})
+	.catch(error => {
+		console.error('Error fetching data:', error);
+	});
