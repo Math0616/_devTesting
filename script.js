be957929b16d411ea80be4e8178bc5a95ee57445
Math0616 <151874665+Math0616@users.noreply.github.com>
2023-12-14 09:00:37 +0100

    update

--- script.js
+++ script.js
@@ -9,27 +9,29 @@ fetch('https://ordinalmaxibiz.vercel.app/api/ombcollection')
 		console.error('Error fetching data:', error);
 	});
 
-document.addEventListener('DOMContentLoaded', function() {
-	fetch('https://ordinalmaxibiz.vercel.app/api/theapegang') // Replace with your actual endpoint
-		.then(response => response.json())
-		.then(tokens => {
-			const container = document.getElementById('token-container');
-			tokens.forEach(token => {
-				const tokenElement = document.createElement('div');
-				tokenElement.classList.add('token');
-
-				// Add token details
-				tokenElement.innerHTML = `
-					<h3>Token ID: ${token.id}</h3>
-					<p>Listed: ${token.listed ? 'Yes' : 'No'}</p>
-					${token.listed ? `<p>Listed Price: ${token.listedPrice}</p>` : ''}
-				`;
-
-				container.appendChild(tokenElement);
+	document.addEventListener('DOMContentLoaded', function() {
+		fetch('https://ordinalmaxibiz.vercel.app/api/theapegang') // Replace with your actual endpoint
+			.then(response => response.json())
+			.then(data => {
+				const container = document.getElementById('token-container');
+				data.forEach(group => {
+					group.tokens.forEach(token => {
+						const tokenElement = document.createElement('div');
+						tokenElement.classList.add('token');
+	
+						// Add token details
+						tokenElement.innerHTML = `
+							<h3>Token ID: ${token.id}</h3>
+							<p>Listed: ${token.listed ? 'Yes' : 'No'}</p>
+							${token.listed ? `<p>Listed Price: ${token.listedPrice}</p>` : ''}
+						`;
+	
+						container.appendChild(tokenElement);
+					});
+				});
+			})
+			.catch(error => {
+				console.error('Error fetching token data:', error);
 			});
-		})
-		.catch(error => {
-			console.error('Error fetching token data:', error);
-		});
-});
+	});	
 	
